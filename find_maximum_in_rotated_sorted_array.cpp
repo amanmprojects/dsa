2c45f250ab130e880c42dc058895e30127461b0b
Aman Mehtar <amanmstudies@gmail.com>
2026-02-19 17:47:50 +0000

    2 pobs

---
+++ find_maximum_in_rotated_sorted_array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        int l = 0, r = nums.size()-1;
+        int curr = nums[l];
+        int mid;
+
+        if(nums.size() == 1) return nums[0];
+        else if (nums.size() == 2) return min(nums[0], nums[1]);
+        
+        if(nums[l] < nums[r]) return nums[l];
+        while(l < r){
+            mid = (l+r)/2;
+            if(nums[mid] < nums[mid-1]) return nums[mid];
+            if(nums[mid] > nums[l]) l = mid+1;
+            else r = mid-1;
+        }
+        return 0;
+    }
+};
