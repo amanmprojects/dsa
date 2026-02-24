07b9bc1cdd88097a6c22c03e3b30292ae635a230
Aman Mehtar <amanmstudies@gmail.com>
2026-02-24 07:21:06 +0530

    Refactor Koko Eating Bananas solution: add debug output and improve code readability

--- koko_eating_bananas.cpp
+++ koko_eating_bananas.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -16,19 +17,25 @@ public:
 
         while(l <= r){
             int mid = (l+r) / 2;
+            cout<<"Mid : "<<mid;
             if(will_eat(mid, h, piles)) {
+                cout<<" Will eat\n";
                 mink = mid;
                 r = mid - 1;
             }
             else {
-                l = mid+1;
+                l = mid + 1;
             }
         }
+
+        return mink;
     }
 
-    bool will_eat (int k, int h, vector<int>& piles) {
+    bool will_eat (int k, int h, vector<int> piles) {
+        long long hours_needed = 0;
         for(int p: piles) {
-            p = p - 
+            hours_needed += (p + k - 1) / k;
         }
-    }
+        return hours_needed <= h;
+    }   
 };
