52286cb21716448ac6a4b427ca5938de8324cf49
Aman Mehtar <amanmstudies@gmail.com>
2026-03-04 00:23:10 +0530

    Add implementation for plusOne function: provide logic to increment a list of digits by one

---
+++ plus_one.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits[-1] += 1
+        res = []
+        p = len(digits)-1
+        carry = 0
+        while p >= 0:
+            res.insert(0,(digits[p]+carry)%10)
+            carry = (digits[p]+carry) // 10
+            p -= 1
+        if carry:
+            res.append(carry)
+
+        return res
+        
