9ca058ae2b53d5703d49d52fdfa6c1ff88318aca
Aman Mehtar <amanmstudies@gmail.com>
2026-03-03 23:07:50 +0530

    Refactor levelOrder function: simplify implementation for level order traversal of a binary tree

--- binary_tree_level_order_traversal.py
+++ binary_tree_level_order_traversal.py
@@ -12,25 +12,28 @@ class TreeNode:
 class Solution:
     res = []
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root: return []
-        elif(root.left is None and root.right is None): return [[root.val]]
-        res = []                                                       
-        q = deque([root])
-        exp = 0
-        c = 1
-        curr_depth = 0
-        while q:
-            curr = q.popleft()
-            if len(res) <= curr_depth: res.append([])
-            res[curr_depth].append(curr.val)
-            if (curr.left): q.append(curr.left)
-            if(curr.right): q.append(curr.right)
-            if(c % (2**exp) == 0): 
-                curr_depth += 1
-                exp += 1
-            c += 1
+        if not root:
+            return []
 
-        return res         
+        res = []
+        queue = deque([root])
+
+        while queue:
+            level_size = len(queue)
+            level = []
+
+            for _ in range(level_size):
+                curr = queue.popleft()
+                level.append(curr.val)
+
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+
+            res.append(level)
+
+        return res
 
         
 
