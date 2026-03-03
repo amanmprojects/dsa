4574861bfc8de2df8d1abecbad1d74886688c71b
Aman Mehtar <amanmstudies@gmail.com>
2026-03-03 23:17:44 +0530

    Add implementation for countGoodNodes algorithm to count good nodes in a binary tree

---
+++ count_good_nodes_in_binary_tree.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def countGoodNodes(self, node: TreeNode, maxParent: int):
+        if not node: return 0
+        return (0 if node.val < maxParent else 1) + self.countGoodNodes(node.left, max(maxParent, node.val)) + self.countGoodNodes(node.right, max(maxParent, node.val))
+        
+    def goodNodes(self, root: TreeNode) -> int:
+        return 1 + self.countGoodNodes(root.left, root.val) + self.countGoodNodes(root.right, root.val)
