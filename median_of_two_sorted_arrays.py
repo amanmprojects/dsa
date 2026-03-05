0d706021be72f4b78e5a13d7c6312035c2e0dac8
Aman Mehtar <amanmstudies@gmail.com>
2026-03-05 22:55:44 +0530

    Add implementation for findMedianSortedArrays function: provide logic to find the median of two sorted arrays

---
+++ median_of_two_sorted_arrays.py
@@ -0,0 +1,13 @@
+from math import ceil
+from typing import List
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        combined = sorted(nums1 + nums2)
+        total = len(combined)
+
+        if total % 2 == 0:
+            return (combined[total // 2 - 1] + combined[total // 2]) / 2
+        
+        return combined[total // 2]     
+
