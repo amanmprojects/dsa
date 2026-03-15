1f47b5d50ba072b826a50121f9c5916e904b7fdc
Aman Mehtar <amanmstudies@gmail.com>
2026-03-15 07:54:43 +0530

    Refactor isValidBST method: improve boundary checks for binary search tree validation

--- validate_binary_search_tree.py
+++ validate_binary_search_tree.py
@@ -11,13 +11,13 @@ class Solution:
     def isValidBST(self, root: Optional[TreeNode], min: int = None, max: int = None) -> bool:
         if root is None: return True 
 
-        if max and not min:
-            if root.val > max: return False
+        if max is not None and min is None:
+            if root.val >= max: return False
             return self.isValidBST(root.left, max = root.val) and self.isValidBST(root.right, max=max, min=root.val)
-        if min and not max:
-            if root.val < min: return False
+        if min is not None and max is None:
+            if root.val <= min: return False
             return self.isValidBST(root.right, min=root.val) and self.isValidBST(root.left, min=min, max=root.val)
-        if min and max:
-            if root.val < min or root.val > max: return False
+        if min is not None and max is not None:
+            if root.val <= min or root.val >= max: return False
             return self.isValidBST(root.left, max=root.val, min=min) and self.isValidBST(root.right, min=root.val, max=max)
         return self.isValidBST(root.left, max=root.val) and self.isValidBST(root.right, min=root.val)
