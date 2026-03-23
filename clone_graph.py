56041c80d1bc5643ab6819e08cc5b25837ef8bc5
Aman Mehtar <amanmstudies@gmail.com>
2026-03-23 22:55:21 +0530

    Implement "Clone Graph"

---
+++ clone_graph.py
@@ -0,0 +1,31 @@
+from os import CLONE_FILES
+from typing import Optional
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+        if node is None:
+            return
+
+        visited = {}
+
+        def dfs(node: Node):
+            if node in visited:
+                return visited[node]
+
+            clone = Node(node.val)
+            visited[node] = clone
+
+            for n in node.neighbors:
+                clone.neighbors.append(dfs(n))
+
+            return clone
+
+        return dfs(node=node)
