42631b377f5cbc9c62a398bf6d24cae967ef297f
Aman Mehtar <127136587+amanmprojects@users.noreply.github.com>
2026-03-29 19:00:19 +0530

    Implement Coin Change

---
+++ coin_change.py
@@ -0,0 +1,27 @@
+from math import inf
+from typing import List
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        coins.sort(reverse=True)
+        memo = {}
+        def dfs(s: int):
+            if s in memo:
+                return memo[s]
+            if s > amount:
+                return inf
+
+            if s == amount:
+                return 0   
+            
+            m = inf
+            for c in coins:
+                m = min(m, 1 + dfs(s + c))
+            
+            memo[s] = m
+            
+            return m
+        
+        res = dfs(0)
+        return res if res != inf else -1
+      
