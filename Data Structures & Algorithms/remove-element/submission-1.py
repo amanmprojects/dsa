62c42c85c8dc076bb8b0a700e76c5fd064428a67
Aman Mehtar <127136587+amanmprojects@users.noreply.github.com>
2026-04-06 21:44:31 +0530

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        if not nums:
+            return 0
+        
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+                continue
+            i += 1
+        
+        return len(nums)
