e466fd75e46d93d8a24fc5e1a3ff5d27e562a96e
Aman Mehtar <127136587+amanmprojects@users.noreply.github.com>
2026-07-03 16:56:29 +0530

    Time: 99 ms (86.61%), Space: 21.2 MB (83.05%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -2,13 +2,13 @@ class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         m, n = len(mat), len(mat[0])
 
-        q = deque([])
-        visit = set()
+        q = deque()
         for r in range(m):
             for c in range(n):
                 if mat[r][c] == 0:
-                    visit.add((r, c))
                     q.append((r, c))
+                else:
+                    mat[r][c] = -1
 
         # Multi-source BFS
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
@@ -18,8 +18,7 @@ class Solution:
                 r, c = q.popleft()
                 for dr, dc in directions:
                     nr, nc = r + dr, c + dc
-                    if 0 <= nr < m and 0 <= nc < n and (nr, nc) not in visit:
-                        visit.add((nr, nc))
+                    if 0 <= nr < m and 0 <= nc < n and mat[nr][nc] == -1:
                         mat[nr][nc] = dist
                         q.append((nr, nc))
 
