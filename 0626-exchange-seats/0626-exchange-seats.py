ebcc359ded912d3dd88465e909faf28a39d40914
Aman Mehtar <127136587+amanmprojects@users.noreply.github.com>
2026-07-04 22:52:35 +0530

    Time: 283 ms (49.5%), Space: 66.5 MB (90.95%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.py
+++ 0626-exchange-seats/0626-exchange-seats.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 def exchange_seats(seat: pd.DataFrame) -> pd.DataFrame:
-    lim = (len(seat) // 2) * 2
+    seat["student"] = seat["student"].where(
+        (seat["id"] % 2 == 0) | (seat["id"] == len(seat)),
+        seat["student"].shift(-1)
+    ).where(
+        (seat["id"] % 2 == 1),
+        seat["student"].shift(1)
+    )
 
-    for i in range(0, lim, 2):
-        temp = seat.at[i, "student"]
-        seat.at[i, "student"] = seat.at[i+1, "student"]
-        seat.at[i+1, "student"] = temp
-    
     return seat
